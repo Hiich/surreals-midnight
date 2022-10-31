eef76fef19fcde1129540b8cf80d1ca940338e4d
unknown <hicham.benhima1@gmail.com>
2022-10-31 22:18:19 +0100

    go live

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const config = {
     networkId: 5,
-    nftAddress: "0x596DB113f3bc6f7e850735a0F6EcEd1aA1163C4d",
-    swapAddress: "0xeFE0Ca3861A540D283DE237Be87163890FD70Dba",
+    nftAddress: "0x7fe24fC7F8ba6DaBb18A8F12Dd277e469A197c7c",
+    swapAddress: "0x28b4A6757a980c8d73c6b65d073097a734655E07",
     darkAddress: "0xa406489360A47Af2C74fc1004316A64e469646A5"
 }
